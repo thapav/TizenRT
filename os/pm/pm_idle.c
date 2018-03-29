29d17356c8c3fdc9057997bf33df0e9a16d8cebb
Vidisha Thapa <thapa.v@samsung.com>
2018-03-29 19:40:49 +0530

    pm: add PM framework
    Adds a generic Idle Power Management framework.
    Rather than having an SoC dependent framework in which
    Soc has to map to number of states provided by the PM framework,
    this patch provides a generic framework where any SoC can map
    its sleep states to the TizenRT power management framework.
    
    It uses SOC provided switch idle state function to switch amongst
    given sleep states.
    
    Signed-off-by: Vidisha Thapa <thapa.v@samsung.com>

--- os/pm/pm_idle.c
+++ os/pm/pm_idle.c
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- * Copyright 2016 Samsung Electronics All Rights Reserved.
+ * Copyright 2018 Samsung Electronics All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,9 +16,7 @@
  *
  ****************************************************************************/
 /****************************************************************************
- *  arch/arm/src/common/up_idle.c
- *
- *   Copyright (C) 2007-2009, 2011 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2011-2012, 2015-2016 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -49,48 +47,92 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *
  ****************************************************************************/
+/****************************************************************************
+ * os/pm/pm_idle.c
+ ****************************************************************************/
 
 /****************************************************************************
  * Included Files
  ****************************************************************************/
 
 #include <tinyara/config.h>
+#include <debug.h>
 
-#include <tinyara/arch.h>
-#include "up_internal.h"
+#include <tinyara/board.h>
+#include <tinyara/pm/pm.h>
+
+#include <tinyara/irq.h>
 
 /****************************************************************************
  * Pre-processor Definitions
  ****************************************************************************/
 
-/****************************************************************************
- * Private Data
- ****************************************************************************/
+#define PM_IDLE_DOMAIN	0
 
 /****************************************************************************
  * Private Functions
  ****************************************************************************/
+/****************************************************************************
+ * Name: pm_idle_process
+ *
+ * Description:
+ *   Perform IDLE state power management.
+ *
+ ****************************************************************************/
+static void pm_idle_process(void)
+{
+	static int oldstate = PM_NORMAL;
+	int newstate;
+	irqstate_t flags;
+	int ret;
+
+	/* Decide, which power saving level can be obtained */
+	newstate = pm_checkstate(PM_IDLE_DOMAIN);
+
+	/* Check for state changes */
+	if (newstate != oldstate) {
+		flags = irqsave();
+
+		/* Then force the global state change */
+		ret = pm_changestate(PM_IDLE_DOMAIN, newstate);
+		if (ret < 0) {
+			/* The new state change failed, revert to the preceding state */
+			pm_changestate(PM_IDLE_DOMAIN, oldstate);
+		} else {
+			/* SOC-specific power management logic */
+			ret = up_pm_switch_state(newstate);
+			if (ret < 0) {
+				/* Staying in same state as SOC returned error */
+				ret = up_pm_switch_state(oldstate);
+				ret = pm_changestate(PM_IDLE_DOMAIN, oldstate);
+			} else {
+				/* Save the new state */
+				oldstate = newstate;
+			}
+		}
+
+		irqrestore(flags);
+	}
+}
 
 /****************************************************************************
  * Public Functions
  ****************************************************************************/
 
 /****************************************************************************
- * Name: up_idle
+ * Name: pm_idle
  *
  * Description:
- *   up_idle() is the logic that will be executed when there is no other
- *   ready-to-run task & no PM framework.  This is processor idle time and
- *   will continue until some interrupt occurs to cause a context switch
- *   from the idle task.
+ *   pm_idle() is the logic that will be executed when their is no other
+ *   ready-to-run task.  This is processor idle time and will continue until
+ *   some interrupt occurs to cause a context switch from the idle task.
  *
- *   Processing in this state may be processor-specific.
+ *   Processing in this state may be processor-specific. e.g., this is where
+ *   power management operations might be performed.
  *
  ****************************************************************************/
-
-void up_idle(void)
+void pm_idle(void)
 {
-	/* Sleep until an interrupt occurs to save power */
-
-	asm("WFI");
+	/* Perform IDLE mode power management */
+	pm_idle_process();
 }
